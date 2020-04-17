e0cca0dc6cfb6b8299a7003d06abf13ce59318f3
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-04-17 19:27:13 +0100

    Add SnapObjects namespace
    - Add Plugins to gitignore

--- Assets/Experiments/SnapObjects/Scripts/GridObjectPlacer.cs
+++ Assets/Experiments/SnapObjects/Scripts/GridObjectPlacer.cs
@@ -3,55 +3,57 @@ using System.Collections.Generic;
 using Shared;
 using UnityEngine;
 
+namespace SnapObjects {
 public class GridObjectPlacer : MonoBehaviour
 {
 
-    [SerializeField]
-    public float gridSize = 5f;
+        [SerializeField]
+        public float gridSize = 5f;
 
-    [SerializeField]
-    private Transform prefab;
+        [SerializeField]
+        private Transform prefab;
 
-    private Transform modelPreview;
+        private Transform modelPreview;
 
-    private Bounds bounds;
+        private Bounds bounds;
 
-    [SerializeField]
-    private RaycastObjectGetter raycastObjectGetter;
+        [SerializeField]
+        private RaycastObjectGetter raycastObjectGetter;
 
-    void Start()
-    {
-        modelPreview = Instantiate(prefab, transform.position, Quaternion.identity);
-        bounds = modelPreview.GetComponent<BoxCollider>().bounds;
-
-        raycastObjectGetter.onRayHit += onRayHit;
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        void Start()
         {
-            Instantiate(prefab, modelPreview.position, Quaternion.identity);
+            modelPreview = Instantiate(prefab, transform.position, Quaternion.identity);
+            bounds = modelPreview.GetComponent<BoxCollider>().bounds;
+
+            raycastObjectGetter.onRayHit += onRayHit;
         }
-    }
 
-    private void OnDestroy()
-    {
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(prefab, modelPreview.position, Quaternion.identity);
+            }
+        }
+
+        private void OnDestroy()
+        {
         raycastObjectGetter.onRayHit -= onRayHit;
-    }
+        }
 
 
-    private void onRayHit(RaycastHit hit)
-    {
-        modelPreview.position = new Vector3(
-            ToGrid(hit.point.x) + bounds.extents.x,
-            hit.point.y,
-            ToGrid(hit.point.z) + bounds.extents.z
-        );
-    }
+        private void onRayHit(RaycastHit hit)
+        {
+            modelPreview.position = new Vector3(
+                ToGrid(hit.point.x) + bounds.extents.x,
+                hit.point.y,
+                ToGrid(hit.point.z) + bounds.extents.z
+            );
+        }
 
-    private float ToGrid(float number)
-    {
-        return Mathf.FloorToInt(number / gridSize) * gridSize;
-    }
+        private float ToGrid(float number)
+        {
+            return Mathf.FloorToInt(number / gridSize) * gridSize;
+        }
+}
 }
