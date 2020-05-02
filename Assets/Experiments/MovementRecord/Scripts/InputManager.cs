6dacd2caf47145a304bb6934e8262ed530d75ba6
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-05-02 22:40:14 +0200

    Add InputManager and UserController classes

---
+++ Assets/Experiments/MovementRecord/Scripts/InputManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MovementRecord
+{
+    public struct VelocityStruct : IRecordable
+    {
+        public float horizontal;
+        public float vertical;
+        public VelocityStruct(float horizontal, float vertical)
+        {
+            this.horizontal = horizontal;
+            this.vertical = vertical;
+        }
+    }
+
+    [RequireComponent(typeof(UserController))]
+    public class InputManager : MonoBehaviour
+    {
+        private RecordManager<VelocityStruct> recordManager = new RecordManager<VelocityStruct>();
+        private PlayBackManager<VelocityStruct> playbackManager = new PlayBackManager<VelocityStruct>();
+        private UserController userController;
+
+        private float lastHorizontal = 0;
+        private float lastVertical = 0;
+
+        private void Update()
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if (playbackManager.isPlayingBack)
+                {
+                    playbackManager.StopPlayBack();
+                }
+                else
+                {
+                    playbackManager.SetRecodedEntries(recordManager.GetEntries());
+                    playbackManager.StartPlayBack();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (recordManager.isRecording)
+                {
+                    recordManager.StopRecord();
+                }
+                else
+                {
+                    recordManager.StartRecord();
+                }
+            }
+
+            if (recordManager.isRecording)
+            {
+                float horizontal = Input.GetAxisRaw("Horizontal");
+                float vertical = Input.GetAxisRaw("Vertical");
+
+                userController.SetVelocities(vertical, horizontal);
+
+                if (horizontal != lastHorizontal || vertical != lastVertical)
+                {
+                    lastHorizontal = horizontal;
+                    lastVertical = vertical;
+
+                    recordManager.AddEntry(new VelocityStruct(horizontal, vertical));
+                }
+            }
+
+            if (playbackManager.isPlayingBack)
+            {
+                List<TimedEntry<VelocityStruct>> newEntries = playbackManager.GetNewPlayBackEntries();
+
+                if (newEntries.Count > 0)
+                {
+                    VelocityStruct entry = newEntries[newEntries.Count - 1].entry;
+
+                    userController.SetVelocities(entry.vertical, entry.horizontal);
+                }
+            }
+        }
+
+        private void Start()
+        {
+            userController = GetComponent<UserController>();
+        }
+    }
+}
---
+++ Assets/Experiments/MovementRecord/Scripts/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MovementRecord
+{
+    public class UserController : MonoBehaviour
+    {
+
+        [SerializeField]
+        private float speed = 5f;
+
+        [SerializeField]
+        private float angularSpeed = 5f;
+
+        private float velocity = 0;
+        private float angularVelocity = 0;
+
+        private Rigidbody rb;
+
+
+        public void SetVelocities(float vertical, float horizontal)
+        {
+            Debug.LogFormat("Setting Velocities to {0} and {1}", vertical, horizontal);
+            this.velocity = vertical;
+            this.angularVelocity = horizontal;
+        }
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            rb.velocity = transform.forward * this.velocity * speed;
+            rb.angularVelocity = transform.up * this.angularVelocity * angularSpeed;
+        }
+    }
+}
