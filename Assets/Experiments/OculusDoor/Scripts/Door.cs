3754fc20b07e7dc6677782f313d09c8a3e169acb
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-09-19 18:01:27 +0200

    Create OculusDoor VR experiment

---
+++ Assets/Experiments/OculusDoor/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OculusDoor
+{
+    public class Door : MonoBehaviour
+    {
+        private Coroutine openDoorRoutine = null;
+
+        [SerializeField]
+        private Animator animator;
+
+        public void Open()
+        {
+            animator.SetBool("open", true);
+            ScheduleClose();
+        }
+        
+        public void ScheduleClose()
+        {
+            if (openDoorRoutine != null)
+            {
+                StopCoroutine(openDoorRoutine);
+            }
+
+            openDoorRoutine = StartCoroutine(OpenDoorCoroutine());
+        }
+
+        public void Close()
+        {
+            animator.SetBool("open", false);
+        }
+
+        private IEnumerator OpenDoorCoroutine()
+        {
+            yield return new WaitForSeconds(2);
+            Close();
+        }
+
+    }
+}
---
+++ Assets/Experiments/OculusDoor/Scripts/DoorOpener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OculusDoor
+{
+    public class DoorOpener : MonoBehaviour
+    {
+        [SerializeField]
+        private Door linkedDoor;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("XRController"))
+            {
+                linkedDoor.Open();
+            }
+        }
+    }
+}
