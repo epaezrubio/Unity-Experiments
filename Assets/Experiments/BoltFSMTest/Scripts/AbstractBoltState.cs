f13142e03384f4a65ccc350d2124e634efc70708
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-11-08 01:52:19 +0000

    Create Bolt FSM test

---
+++ Assets/Experiments/BoltFSMTest/Scripts/AbstractBoltState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BoltFSMTest
+{
+    abstract public class AbstractBoltState : MonoBehaviour
+    {
+        abstract public void OnEnterState();
+        abstract public void OnUpdateState();
+        abstract public void OnExitState();
+    }
+}
---
+++ Assets/Experiments/BoltFSMTest/Scripts/MovingSphereState.cs
@@ -0,0 +1,33 @@
+﻿using Bolt;
+using Ludiq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BoltFSMTest
+{
+    public class MovingSphereState : AbstractBoltState
+    {
+        private StateMachine stateMachine;
+        private GraphReference graphReference;
+
+        public override void OnEnterState()
+        {
+            stateMachine = GetComponent<StateMachine>();
+            graphReference = GraphReference.New(stateMachine, true);
+        }
+
+        public override void OnUpdateState()
+        {
+            transform.position = new Vector3(
+                Mathf.Sin(Time.time) * Variables.ActiveScene.Get<float>("x_radius"),
+                0,
+                Mathf.Cos(Time.time) * Variables.ActiveScene.Get<float>("z_radius"));
+        }
+
+        public override void OnExitState()
+        {
+            Debug.Log("ON EXIT STATE");
+        }
+    }
+}
